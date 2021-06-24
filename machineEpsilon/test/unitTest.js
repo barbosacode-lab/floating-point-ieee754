c30c0d560d06a072ed0a193b69e72ee298a6e7ed
Leandro Barbosa <46463623+lbarbosadata@users.noreply.github.com>
2021-06-24 17:42:46 -0300

    comment line inserted below to test unit
    Inserted comment line to not run the break test for single precision

--- machineEpsilon/test/unitTest.js
+++ machineEpsilon/test/unitTest.js
@@ -10,4 +10,4 @@ assert.deepStrictEqual(roundoff, Number.EPSILON || new Error);
 assert.deepStrictEqual(roundoff, Math.pow(2, -52) || new Error); // pass for double precision
 
 // broken
-assert.deepStrictEqual(roundoff, Math.pow(2, -23) || new Error); // broken for single precision 
+// assert.deepStrictEqual(roundoff, Math.pow(2, -23) || new Error); // broken for single precision 
