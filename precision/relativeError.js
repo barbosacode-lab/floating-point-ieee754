721332783dac5ff754ef20736a1cffbce446fdc3
Leandro Barbosa <leandro.barbosamkt@gmail.com>
2021-06-30 15:46:19 -0300

    [Function: relativeError]
    Calculates the relative error percentage for a rounding

--- precision/relativeError.js
+++ precision/relativeError.js
@@ -3,5 +3,11 @@ const positiveNormalizedDoublePrecision = e => {
     return 2 ** -(e - 1)
 };
 
+const relativeError = (x, y) => {
+    // Scientific notation => (|X - Y| / x)
+    const diff = x - y;
+    return `${Math.abs(diff) / x * 100 | 1}%`
+}
 
-module.exports = { positiveNormalized }
+
+module.exports = { positiveNormalized, relativeError }
